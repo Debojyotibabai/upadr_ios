fa3458d30ff78ba198cdc5a47dfd5f02915988c8
Debojyotibabai <debojyotighosh060@gmail.com>
2025-03-11 08:07:41 +0530

    Initial Commit

---
+++ upadr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  upadr
+//
+//  Created by Debojyoti Ghosh on 11/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ upadr/upadrApp.swift
@@ -0,0 +1,17 @@
+//
+//  upadrApp.swift
+//  upadr
+//
+//  Created by Debojyoti Ghosh on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct upadrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
